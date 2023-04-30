fff0f20f3150aa1c9129ec112c9cc1849d9d99fd
Emy Ifeora <emyboybeats@gmail.com>
2023-04-30 03:27:34 +0100

    fixed

--- src/components/Layouts/MainLayout/MainLayout.tsx
+++ src/components/Layouts/MainLayout/MainLayout.tsx
@@ -11,7 +11,7 @@ export default function MainLayout({ children }: Props) {
         <div className=' '>
             <MainNavbar />
 
-            <div className='bg-background- flex-1 overflow-y-auto flex justify-center'>
+            <div className='bg-background- flex-1 overflow-y-auto flex justify-center pb-16'>
                 <div className='flex h-full- justify-between w-screen lg:w-screen xl:w-2/3  px-0'>
                     <aside className='border-r border-1 w-1/3 hidden lg:visible justify-end lg:flex bg-purple-200- ' >
                         <div className='bg-green-100- fixed  h-96 w-56'>
