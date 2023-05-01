61cce6e34346993699ae534f4c46307a2032f6f6
Emy Ifeora <emyboybeats@gmail.com>
2023-05-01 03:09:55 +0100

    fixed update

--- src/components/Pages/RequestDetails/RequestDetailsTab.tsx
+++ src/components/Pages/RequestDetails/RequestDetailsTab.tsx
@@ -7,7 +7,7 @@ type Props = {}
 export default function RequestDetailsTab({ }: Props) {
     return (
         <div className='h-full'>
-            <ul className='h-full flex gap-0'>
+            <ul className='h-full flex gap-0 overflow-x-auto'>
                 <EachTab title='Summary' active />
                 <EachTab title='Discussions' />
                 <EachTab title='Inspection' />
@@ -19,7 +19,7 @@ export default function RequestDetailsTab({ }: Props) {
 
 const EachTab = ({ title, active }: { title: string, active?: boolean }) => {
     return <li className='h-full'>
-        <Link href={`/`} className={classNames({ "border-theme font-semibold bg-white": active }, { "border-background font-normal text-dark_lighter bg-background": !active }, 'border-b-4  h-full py-2 px-4 flex flex-col justify-center items-center w-fit ')}>
+        <Link href={`/`} className={classNames({ "border-theme font-semibold bg-white": active }, { "border-background font-normal text-dark_lighter bg-background": !active }, 'border-b-4  h-full py-2 px-4 flex flex-col justify-center items-center w-fit text-sm')}>
             <span>{title}</span>
         </Link>
     </li>
