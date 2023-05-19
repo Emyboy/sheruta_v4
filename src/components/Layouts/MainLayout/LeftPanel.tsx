8f662b285b5b4632d9ab43f4ef35599fbc11172e
Emy Ifeora <emyboybeats@gmail.com>
2023-05-19 01:49:49 +0100

    update

--- src/components/Layouts/MainLayout/LeftPanel.tsx
+++ src/components/Layouts/MainLayout/LeftPanel.tsx
@@ -1,21 +1,29 @@
 'use client'
-import React from 'react'
+import React, { useEffect } from 'react'
 import { useSelector, useDispatch } from 'react-redux'
 import { AppStore } from '@/interface/index.interface'
 import MainLeftNav from './MainLeftNav'
 import { setViewState } from '@/redux/features/view.slice'
 
-type Props = {}
 
 export default ({ leftComponent }: { leftComponent: any }) => {
 	const { show_left_panel } = useSelector((state: AppStore) => state.view)
 	const dispatch = useDispatch()
 
+	useEffect(() => {
+		if(window && window.innerWidth > 500){
+			dispatch(setViewState({ show_left_panel: true }))
+		}else {
+			dispatch(setViewState({ show_left_panel: false }))
+		}
+		console.log(window.innerWidth)
+	},[])
+
 	return (
 		<>
 			<aside
-				className="border-r border-1 z-50 justify-end lg:flex  bg-white md:bg-none fixed md:relative md:h-auto h-screen w-72 shadow-lg md:shadow-none md:w-1/3"
-				style={{ left: show_left_panel ? 0 : -500 }}
+				className="border-r border-1 z-50 md:z-0 justify-end lg:flex  bg-white md:bg-none  md:h-auto h-screen w-72 shadow-lg md:shadow-none md:w-1/3"
+				style={{ left: show_left_panel ? 0 : -500,  }}
 			>
 				<div className="bg-green-100- fixed  h-96 w-60">
 					{leftComponent || <MainLeftNav />}
