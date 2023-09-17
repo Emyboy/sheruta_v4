55372a31d30912b1cb525bd778cc85d80d9669a9
Emy Ifeora <emyboybeats@gmail.com>
2023-09-17 20:44:10 +0100

    added nextjs loader

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -7,6 +7,7 @@ import { QueryClientProvider, QueryClient } from 'react-query'
 import { Toaster } from 'react-hot-toast'
 import '../styles/globals.css'
 import { CacheProvider, ChakraProvider } from '@/components/ChakraUi'
+import { Next13ProgressBar } from 'next13-progressbar'
 
 const queryClient = new QueryClient()
 
@@ -19,6 +20,12 @@ export default function providers({ children }: any) {
 						<Provider store={store}>
 							<Toaster />
 							<MasterPopups />
+							<Next13ProgressBar
+								height="4px"
+								color="#00BC73"
+								options={{ showSpinner: true }}
+								showOnShallow
+							/>
 							{children}
 						</Provider>
 					</QueryClientProvider>
